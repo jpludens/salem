19c0a4fdfb849b80dfc8e6bb4100374997db1010
jpalm <palm.jay@gmail.com>
2016-06-29 20:05:22 -0400

    Added settings, lists, and selection behaviors for team and specification restrictions to the controller. Commented out previous select/match controller code.

--- static/js/salemApp.js
+++ static/js/salemApp.js
@@ -10,43 +10,80 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
 app.controller("mainCtrl", function ($scope, roleDescsFactory) {
 	$scope.descriptions = roleDescsFactory();
 
-	$scope.selectedRole = null;
-	$scope.selectedAlignment = null;
+	$scope.specRest = null;
+	$scope.teamRest = null;
+	
+	$scope.specRestValues = ["Roles", "Alignments"];
+	$scope.teamRestValues = ["Town", "Neutral", "Mafia"]
 
-	$scope.selectRole = function (role) {
-		if (role == $scope.selectedRole) {
-			$scope.selectedRole = null;
+	$scope.updateSpecRest = function(clicked) {
+		// If what's already clicked is clicked again,
+		// unselect it and remove this restriction.
+		// Otherwise, change restriction to the new value.
+		if (clicked == null) {
+			return;
+		}
+		if (clicked == $scope.specRest) {
+			$scope.specRest = null;
 		}
 		else {
-			$scope.selectedRole = role;
+			$scope.specRest = clicked;
 		}
 	}
 
-	$scope.selectAlignment = function (alignment) {
-		if (alignment == $scope.selectedAlignment) {
-			$scope.selectedAlignment = null;
+	$scope.updateTeamRest = function(clicked) {
+		// If what's already clicked is clicked again,
+		// unselect it and remove this restriction.
+		// Otherwise, change restriction to the new value.
+		if (clicked == null) {
+			return;
+		}
+		if (clicked == $scope.teamRest) {
+			$scope.teamRest = null;
 		}
 		else {
-			$scope.selectedAlignment = alignment;
+			$scope.teamRest = clicked;
 		}
+
 	}
 
-	$scope.roleMatchesAlignment = function (role, alignment) {
+	// $scope.selectedRole = null;
+	// $scope.selectedAlignment = null;
 
-		if (role == null || alignment == null) {
-			return false
-		}
-		else if (alignment == $scope.alignments.ANY) {
-			return true;
-		}
-		else if (alignment.team == role.team &&
-			    (alignment.category == role.category ||
-				 alignment.category == 'Random')) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+	// $scope.selectRole = function (role) {
+	// 	if (role == $scope.selectedRole) {
+	// 		$scope.selectedRole = null;
+	// 	}
+	// 	else {
+	// 		$scope.selectedRole = role;
+	// 	}
+	// }
+
+	// $scope.selectAlignment = function (alignment) {
+	// 	if (alignment == $scope.selectedAlignment) {
+	// 		$scope.selectedAlignment = null;
+	// 	}
+	// 	else {
+	// 		$scope.selectedAlignment = alignment;
+	// 	}
+	// }
+
+	// $scope.roleMatchesAlignment = function (role, alignment) {
+
+	// 	if (role == null || alignment == null) {
+	// 		return false
+	// 	}
+	// 	else if (alignment == $scope.alignments.ANY) {
+	// 		return true;
+	// 	}
+	// 	else if (alignment.team == role.team &&
+	// 		    (alignment.category == role.category ||
+	// 			 alignment.category == 'Random')) {
+	// 		return true;
+	// 	}
+	// 	else {
+	// 		return false;
+	// 	}
+	// }
 
 });
