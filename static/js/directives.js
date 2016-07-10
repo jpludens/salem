36d843c3bf00711385ff3602c9748089c7d08416
jpalm <palm.jay@gmail.com>
2016-07-10 19:10:23 -0400

    Refactored directive to use text only for most personas, and only add a span when category needs to be colored separately. Link to wiki is now triggered by a link-to-wiki attribute rather than included by default.

--- static/js/directives.js
+++ static/js/directives.js
@@ -6,43 +6,61 @@ app.directive("role", function () {
 	return {
 		scope: '@',
 		link: function (scope, elem, attrs) {
+
+			var getWikiUrl = function (persona) {
+				if (persona == null) {
+					return;
+				}
+
+				var wikiLinkTemplate = 'http://town-of-salem.wikia.com/wiki/';
+
+				if (persona.specificity == 0) {
+					return wikiLinkTemplate + "Roles";
+				}
+				else if (persona.specificity == 1) {
+					return wikiLinkTemplate + persona.team;
+				}
+				else if (persona.specificity == 1) {
+					return wikiLinkTemplate + persona.team + "_" +
+						persona.category;
+				}
+				else if (persona.specificity == 3) {
+					return wikiLinkTemplate + persona.name.replace(" ", "_")
+				}
+				return;
+			}
+
 			if (scope.persona == undefined) {
 				return;
 			}
 
-			var wikiLinkTemplate = 'http://town-of-salem.wikia.com/wiki/';
-
+			
+			// Handle 'Any'
+			if (scope.persona.specificity == 0) {
+				elem.text("Any");
+				elem.addClass("role-text--any");
+			}
 			// Handle exact role names
-			if (scope.persona.specificity == 3) {
+			else if (scope.persona.specificity == 3) {
 				if (scope.persona.team == 'Neutral') {
-					// Generalized handling of transforming sk name to class
+					// Replace space in 'Serial Killer' with a -
+					// Serial Killer is the only 2-word role except
+					// Vampire Hunter - which belongs to the town color class.
 					var classMod =
 						scope.persona.name.toLowerCase().replace(" ", "-")
 				}
 				else {
 					var classMod = scope.persona.team.toLowerCase();
 				}
-				var roleSpan = angular.element("<span></span>");
-				roleSpan.text(scope.persona.name);
-				roleSpan.addClass("role-text--" + classMod);
-				elem.append(roleSpan);
-				attrs.$set('href', wikiLinkTemplate +
-					scope.persona.name.replace(" ", "_"));
-			}
-			// Handle 'Any'
-			else if (scope.persona.specificity == 0) {
-				var roleSpan = angular.element("<span></span>");
-				roleSpan.addClass("role-text--any");
-				roleSpan.text("Any");
-				elem.append(roleSpan);
-				attrs.$set('href', wikiLinkTemplate + "Roles");
+
+				elem.text(scope.persona.name);
+				elem.addClass("role-text--" + classMod);
 			}
 			// Handle 'Random $Team' and '$Team $Category'
 			else {
-				var teamSpan = angular.element("<span></span>");
-				teamSpan.text(scope.persona.team);
-				teamSpan.addClass("role-text--" +
-					scope.persona.team.toLowerCase());
+				// The more general information is team, so set that at top level
+				elem.text(scope.persona.team);
+				elem.addClass("role-text--" + scope.persona.team.toLowerCase())
 
 				var catSpan = angular.element("<span></span>");
 				catSpan.text(scope.persona.category || "Random");
@@ -50,19 +68,27 @@ app.directive("role", function () {
 
 				// Random $Team
 				if (scope.persona.specificity == 1) {
-					elem.append(catSpan);
-					elem.append("&nbsp;");
-					elem.append(teamSpan);
-					attrs.$set('href', wikiLinkTemplate + scope.persona.team);
+					elem.prepend("&nbsp;")
+					elem.prepend(catSpan);
 				}
 				// $Team $Category
 				else {
-					elem.append(teamSpan);
 					elem.append("&nbsp;");
 					elem.append(catSpan);
-					attrs.$set('href', wikiLinkTemplate +
-						scope.persona.team + "_" + scope.persona.category);
 				}
+			}
+			console.log(attrs);
+
+			if (attrs["linkToWiki"]) {
+				// Create an anchor tag linking to the appropriate wiki page
+				var anchor = angular.element("<a></a>")
+				var href = getWikiUrl(scope.persona);
+				anchor.attr('href', href);
+				anchor.attr('target', "_blank")
+				// Wrap the persona element in the anchor tag
+				elem.after(anchor);
+				elem.detach();
+				anchor.append(elem);
 			}
 		}
 	}
